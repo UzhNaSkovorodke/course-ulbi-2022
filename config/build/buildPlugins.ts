3998d980d22beaf2405d8e6fd8dfd61d599a2f30
Тюкалов Кирилл <tiukalov@realx.tech>
2023-10-11 16:02:27 +0300

    15 интернационализация i18

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,18 +1,21 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
+import {BuildOptions} from './types/config'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
 
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        })
-    ]
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(process.env.NODE_ENV !== 'production')
+    })
+  ]
 }
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,17 +1,20 @@
 declare module '*.scss' {
-    interface IClassNames {
-        [className: string]: string
-    }
-    const classNames: IClassNames;
-    export = classNames;
+  interface IClassNames {
+    [className: string]: string
+  }
+
+  const classNames: IClassNames
+  export = classNames;
 }
 
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg' {
-    import React from 'react';
+declare module '*.png'; //нужно чтобы TypeScript мог работать с PNG
+declare module '*.jpg'; //нужно чтобы TypeScript мог работать с JPG
+declare module '*.jpeg'; //нужно чтобы TypeScript мог работать с JPEG
+declare module '*.svg' { //нужно чтобы TypeScript мог работать с SVG
+  import React from 'react'
 
-    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-    export default SVG;
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
 }
+declare const __IS_DEV__: boolean // нужно для того чтобы TypeScript видел, что в этой переменной будет храниться логический тип
+// в этом файле перечислены типы которые используются в проекте
