68bd3b59b7628607764292245d9304715548c93b
Тюкалов Кирилл <tiukalov@realx.tech>
2023-10-20 17:27:50 +0300

    feat/правки NavBar.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import { Link } from 'react-router-dom'
 import './styles/index.css'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from 'app/providers/router'
+import { Navbar } from '../widgets/Navbar'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   )
 }
