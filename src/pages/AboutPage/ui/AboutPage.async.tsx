3cb212c90d392e4fb1f999990ac807e823e35bce
Тюкалов Кирилл <tiukalov@realx.tech>
2023-10-25 10:43:05 +0300

    feat/github_actions_test

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -5,6 +5,6 @@ export const AboutPageAsync = lazy(
     new Promise((resolve) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      setTimeout(() => resolve(import('./AboutPage')), 1500)
+      setTimeout(() => resolve(import('./AboutPage')), 0)
     })
 )
